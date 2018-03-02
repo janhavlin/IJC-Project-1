6878b55c680201114802b368d209b5171c84fd96
Jan Havlín <xhavli47@stud.fit.vutbr.cz>
2018-03-02 15:23:10 +0100

    Přidány makra pro bitové pole

---
+++ bit_array.h
@@ -0,0 +1,14 @@
+// Reseni IJC-DU1, priklad a), 2018-03
+// Autor: Jan Havlin, FIT
+// Prelozeno:
+// -popis prikladu-
+
+typedef bit_array_t unsigned long[];
+
+#define bit_array_create(jmeno_pole, velikost)
+
+#define bit_array_size(jmeno_pole)
+
+#define bit_array_setbit(jmeno_pole, index, vyraz)
+
+#define bit_array_getbit(jmeno_pole, index)
