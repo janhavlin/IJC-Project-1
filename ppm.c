e8d512d4fb1394854e90ebfce7584756ff0635f0
Jan Havlín <xhavli47@stud.fit.vutbr.cz>
2018-03-18 00:19:39 +0100

    Pridani write, prepsani read

--- ppm.c
+++ ppm.c
@@ -6,7 +6,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ppm.h"
+#include "error.h"
 
+/*
 struct ppm * ppm_read(const char * filename)
 {
 	int c;
@@ -14,26 +16,29 @@ struct ppm * ppm_read(const char * filename)
 	file = fopen(filename, "r");
 	if (file)
 	{
+		// Cteni po jednotlivych znacich je ponekud osklive, ale pro 8 znaku se to da prezit
 		if ((c = getc(file)) == 'P' && (c = getc(file)) == '6' && (c = getc(file)) == '\n')
 		{
-
-			char tmp_str[32];
-			fgets(tmp_str, 32, file);
-			// puts(tmp_str);
-			struct ppm* img;
-			img = (struct ppm *)malloc(sizeof(struct ppm));
-			sscanf(tmp_str, "%u %u\n", &(img->xsize), &(img->ysize));
-
-			// img->data = (char*)malloc(sizeof(char) * 3 * img->xsize * img->ysize);
-			// puts("BLABLA");
-			// printf("%u %u\n", img->xsize, img->ysize);
+			unsigned tmp_x, tmp_y;
 			
-			if ((c = getc(file)) == '2' && (c = getc(file)) == '5' && (c = getc(file)) == '5' && (c = getc(file)) == '\n')
+			// Nacteni hodnot X, Y 
+			fscanf(file, "%u %u", &tmp_x, &tmp_y);
+			struct ppm* img;
+			img = (struct ppm *)malloc(sizeof(struct ppm) + 3 * tmp_x * tmp_y);
+			
+			if (img == NULL)
 			{
-				// puts("BLABLABLA");
+				fclose(file);	
+				error_exit("Alokovani pameti pro strukturu obrazku.\n");	
+			}
+			
+			img->xsize = tmp_x;
+			img->ysize = tmp_y;
+
+			if ((c = getc(file)) == '\n' && (c = getc(file)) == '2' && (c = getc(file)) == '5' && (c = getc(file)) == '5' && (c = getc(file)) == '\n')
+			{
 				unsigned i = 0;
-				while ((c = getc(file)) != EOF && i < 3 * img->xsize * img->ysize 
-						&& i < 100000)	// TOTO SMAZAT
+				while ((c = getc(file)) != EOF && i < 3 * img->xsize * img->ysize)
 				{
 					// printf("%u ", i);
 					//putchar(c);
@@ -41,24 +46,110 @@ struct ppm * ppm_read(const char * filename)
 					// putchar(img->data[i]);
 					i++;
 				}
+				fclose(file);
 				return img;
+			} 
+			else // Podminka hodnoty barevne hloubky
+			{
+			free(img);
+			fclose(file);	
+			warning_msg("PPM soubor neobsahuje barevnou hloubku 255.\n");				
 			}
-			else
-			puts("ERROR");				
+		} 
+		else // Podminka prvnich 3 znaku P6\n
+		{
+		fclose(file);	
+		warning_msg("Soubor neni ve formatu P6.\n");
 		}
-		else
-		puts("ERROR");
-		// while ((c = getc(file)) != '5')
-			// putchar(c);
-		// fclose(file);
+
+	} // Podminka nacteni souboru (file != NULL)
+	else
+	{
+	warning_msg("Chyba pri cteni souboru.\n");
 	}
-	puts("ERROR");
 	return NULL;
+}*/
+
+struct ppm * ppm_read(const char * filename)
+{
+	int c;
+	FILE *file;
+	file = fopen(filename, "r");
+	if (!file)
+	{
+		warning_msg("Chyba pri otevirani souboru.\n");
+		return NULL;
+	}
+
+	// Cteni po jednotlivych znacich je ponekud osklive, ale pro 8 znaku se to da prezit
+	if ( !((c = getc(file)) == 'P' && (c = getc(file)) == '6' && (c = getc(file)) == '\n') )
+	{
+		fclose(file);	
+		warning_msg("Soubor neni ve formatu P6.\n");
+		return NULL;
+	}
+	
+	unsigned tmp_x, tmp_y;
+	
+	// Nacteni hodnot X, Y 
+	fscanf(file, "%u %u", &tmp_x, &tmp_y);
+	struct ppm* img;
+	img = (struct ppm *)malloc(sizeof(struct ppm) + 3 * tmp_x * tmp_y);
+	
+	if (img == NULL)
+	{
+		fclose(file);	
+		error_exit("Alokovani pameti pro strukturu obrazku.\n");
+		return NULL;
+	}
+	
+	img->xsize = tmp_x;
+	img->ysize = tmp_y;
+
+	if ( !((c = getc(file)) == '\n' && (c = getc(file)) == '2' && (c = getc(file)) == '5' && (c = getc(file)) == '5' && (c = getc(file)) == '\n') )
+	{
+		free(img);
+		fclose(file);	
+		warning_msg("PPM soubor neobsahuje barevnou hloubku 255.\n");
+		return NULL;
+	}
+	
+	unsigned i = 0;
+	while ((c = getc(file)) != EOF && i < 3 * img->xsize * img->ysize)
+	{
+		// printf("%u ", i);
+		//putchar(c);
+		img->data[i] = c;
+		// putchar(img->data[i]);
+		i++;
+	}
+	fclose(file);
+	return img;
 }
 
-// int main(int argc, char *argv[])
-// {
-// if (argc){};
-	// ppm_read(argv[1]);
-	// return 0;
-// }
+int ppm_write(struct ppm *p, const char * filename)
+{
+	if (p == NULL)
+	{
+		warning_msg("Nelze zapisovat do souboru, ukazatel == NULL.\n");
+		return -1;
+	}
+		
+	FILE *file = fopen(filename, "w");
+	if (file == NULL)
+	{
+		warning_msg("Chyba pri vytvareni souboru.\n");
+		return -1;		
+	}
+	fprintf(file, "P6\n%u %u\n255\n", p->xsize, p->ysize);
+	
+	unsigned count = fwrite(p->data, sizeof(char), 3 * p->xsize * p->ysize, file);
+	if (count != 3 * p->xsize * p->ysize)
+	{
+		warning_msg("Chyba pri zapisu do souboru.\n");
+		return -1;	
+	}
+	
+	fclose(file);
+	return 1;
+}
